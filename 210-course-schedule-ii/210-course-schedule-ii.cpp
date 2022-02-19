ee0a9ec0ac52ffbcbcea3235af57cac60c3fa66e
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-02-19 03:18:08 -0500

    Time: 42 ms (19.49%), Space: 13.9 MB (62.61%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,36 +1,69 @@
 class Solution {
+     //Graph coloring: 0->not visited...1->visited...2->visited & processed
+    bool detectCycle_util(vector<vector<int>>& adj,vector<int>& visited,int v)
+    {
+        if(visited[v]==1)
+            return true;
+        if(visited[v]==2)
+            return false;
+        
+        visited[v]=1;   //Mark current as visited
+        for(int i=0;i<adj[v].size();++i)
+            if(detectCycle_util(adj,visited,adj[v][i]))
+                return true;
+        
+        visited[v]=2;   //Mark current node as processed
+        return false;
+    }
+    
+    //Cycle detection
+    bool detectCycle(vector<vector<int>>& adj,int n)
+    {
+        vector<int> visited(n,0);
+        for(int i=0;i<n;++i)
+            if(!visited[i])
+                if(detectCycle_util(adj,visited,i))
+                    return true;
+        return false;
+    }
+    
+    //Topological sort
+    void dfs(vector<vector<int>>& adj,int v,vector<bool>& visited,stack<int>& mystack)
+    {
+        visited[v] = true;
+        for(int i=0;i<adj[v].size();++i)
+            if(!visited[adj[v][i]])
+                dfs(adj,adj[v][i],visited,mystack);
+        
+        mystack.push(v);
+    }
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-       vector<vector<int>>graph(numCourses);
-        vector<int>bfs;
-        vector<int>degree(numCourses,0);
+        int n=prerequisites.size();
+        vector<vector<int>> adj(numCourses);
+        //Make adjacecncy list
+        for(int i=0;i<n;++i)
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         
-        for(int i=0;i<prerequisites.size();i++)
-        {
-            graph[prerequisites[i][1]].push_back(prerequisites[i][0]);
-            degree[prerequisites[i][0]]++;
-        }
-        for(int i=0;i<numCourses;i++)
-        {
-            if(!degree[i])
-            {
-                bfs.push_back(i);
-            }
-        }
+        //Detect CYCLE...If present then return empty array
+        vector<int> ans;
+        if(detectCycle(adj,numCourses))
+            return ans;        
         
-        for(int i=0;i<bfs.size();i++)
+        //Find toposort and store it in stack
+        stack<int> mystack;
+        vector<bool> visited(numCourses,false);
+        
+        //Apply DFS and find topological sort
+        for(int i=0;i<numCourses;++i)
+            if(!visited[i])
+                dfs(adj,i,visited,mystack);
+        
+        while(!mystack.empty())
         {
-            
-            for(auto el:graph[bfs[i]])
-            {
-                degree[el]--;
-                if(degree[el]==0)
-                {
-                    bfs.push_back(el);
-                }
-            }
+            ans.push_back(mystack.top());
+            mystack.pop();
         }
-        vector<int>ans;
-        return bfs.size()==numCourses?bfs:ans;
+        return ans;
     }
 };
