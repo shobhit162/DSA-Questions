0a26f9b6fc0c893d36e02fc88c3338a51c2cb0d6
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-02-25 12:40:13 -0500

    Added solution - LeetHub

--- Robots - GFG/robots.cpp
+++ Robots - GFG/robots.cpp
@@ -10,18 +10,24 @@ using namespace std;
 class Solution{   
 public:
     string moveRobots(string s1, string s2){
-        // code here 
-         if (s1.length() != s2.length()) return "No";
-        int a1 = 0, b2 = 0;
-        for(int i = 0; i < s1.length(); i++)
-        {
-            if (s1[i] == 'A') a1++;
-            if (s2[i] == 'A') a1--; 
-            if (s1[i] == 'B') b2--;
-            if (s2[i] == 'B') b2++;
-            if (a1 > 0 || b2 > 0 || (b2 < 0 && a1 < 0)) return "No";
-        }
-        return "Yes";
+           // code here 
+       int p1=0,p2=0,n=s1.length();
+       while(p1<n && p2<n){
+           if(s1[p1]=='#') // passing empty spaces of s1
+               p1++;
+           else if(s2[p2]=='#') // passing empty spaces of s2
+               p2++;
+           else if(s1[p1]!=s2[p2]) // non-empty spaces are not captured by similar bots
+               return "No";
+           else if(s1[p1]=='A' && p1<p2) // bot A appears left of it
+               return "No";
+           else if(s1[p1]=='B' && p1>p2) // bot B appears right of it
+               return "No";
+           else{   // bots are there where they should be
+               p1++;p2++;
+           }
+       }
+       return "Yes";
     }
 };
 
