5b2abe3465f0e11800a881771f7818aabb563847
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-03-01 01:21:39 -0500

    Time: 5 ms (68.32%), Space: 7.9 MB (48.88%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-         // n+1 as we are going to count from 0 to n
+        // n+1 as we are going to count from 0 to n
         vector<int> t(n+1);
         
         // t[0] will be 0 beacuse 0 has count of set bit is 0;
@@ -10,7 +10,7 @@ public:
         // we can compute current set bit count using previous count
         // as x/2 in O(1) time
         
-        // i%2 will be 0 for even number ans 1 for odd number
+        // i%2 will be 0 for even number and 1 for odd number
         
         for(int i = 1; i<=n; ++i)
             t[i] = t[i/2] + i%2;
