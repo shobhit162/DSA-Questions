4663b1abf7fc8b36dedf8288c2331ba2638bb809
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-03-01 01:20:46 -0500

    Time: 14 ms (16.41%), Space: 7.9 MB (48.88%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-         vector<int> ans;
-		
-		// iterating fromt 0 to n
-        for(int i = 0; i<=n; i++)
-        {
-			// sum is initialised as 0
-            int sum = 0;
-            int num = i;
-			// while num not equals zero
-            while(num != 0)
-            {
-				// we have to count 1's in binary representation of i, therefore % 2
-                sum += num%2;
-                num = num/2;
-            }
-			// add sum to ans vector
-            ans.push_back(sum);
-        }
-		// return 
-        return ans;
+         // n+1 as we are going to count from 0 to n
+        vector<int> t(n+1);
+        
+        // t[0] will be 0 beacuse 0 has count of set bit is 0;
+        t[0] = 0;
+        
+        // we can compute current set bit count using previous count
+        // as x/2 in O(1) time
+        
+        // i%2 will be 0 for even number ans 1 for odd number
+        
+        for(int i = 1; i<=n; ++i)
+            t[i] = t[i/2] + i%2;
+        
+        return t;
     }
 };
