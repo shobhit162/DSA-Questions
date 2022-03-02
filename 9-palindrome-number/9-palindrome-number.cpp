86f809640eb74fec1ae0f73be18e1ef4017f5073
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-03-02 11:56:03 -0500

    Time: 15 ms (69.76%), Space: 5.7 MB (91.41%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s=to_string(x);
+        int i=0;
+        int j=s.size()-1;
+        while(i<j){
+            if(s[i]==s[j]){
+                i++;
+                j--;
+            }
+            else return false;
+        }
+        return true;
+    }
+};
