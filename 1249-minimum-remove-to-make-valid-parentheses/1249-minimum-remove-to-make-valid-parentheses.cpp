9fc660d716bc151857eb487e4c24dbd7e1d2f000
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-03-15 03:56:19 -0400

    Time: 13 ms (98.77%), Space: 11.7 MB (42.01%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,47 +1,35 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        int n = s.length();
+       stack<int> st;
         string res = "";
 
-        int cnt = 0;
-        // iterating from beginnning
         for (int i = 0; i < s.length(); ++i)
         {
-            // if we find and open parenthesis increase the cnt
+            // if we find open parenthesis push it to stack
             if (s[i] == '(')
-                ++cnt;
-            // if we found an close parenthisis
-            // check if cnt == 0 , that means we have no earlier open parenthesis,
-            // therefore replace that index by '#'
+                st.push(i);
+            // if we find close parenthisis check
+            // if there is a open parenthisis or the stack is empty
             else if (s[i] == ')')
             {
-                if (cnt == 0)
+                // if stack  is empty then we need to remove that index from string
+                // replacing it with '#' for our convenience
+                if (st.empty())
                     s[i] = '#';
-                // else decrease the cnt beacuse a valid pair is found
                 else
-                    --cnt;
+                    // if there is an open parenthesis than remove that  from the stack
+                    st.pop();
             }
         }
 
-        cnt = 0;
-        // iterating from the end
-        for (int i = n - 1; i >= 0; --i)
+        // now , if stack is not empty that means it has open parenthesis
+        // which have no closing parenthesis.
+        // so we have to replace that index of string with '#' for our convenience
+        while (!st.empty())
         {
-            // if we find and close parenthesis increase the cnt
-            if (s[i] == ')')
-                ++cnt;
-            // if we found an open parenthisis
-            // check if cnt == 0 , that means we have no earlier close parenthesis,
-            // therefore replace that index by '#'
-            else if (s[i] == '(')
-            {
-                if (cnt == 0)
-                    s[i] = '#';
-                // else decrease the cnt beacuse a valid pair is found
-                else
-                    --cnt;
-            }
+            s[st.top()] = '#';
+            st.pop();
         }
 
         // making the resultant string by excluding '#'
