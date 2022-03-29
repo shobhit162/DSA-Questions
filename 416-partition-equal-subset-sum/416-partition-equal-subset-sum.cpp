49fd6bbaa74b7eb2e229ac0be36f813f522fb1c5
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-03-29 16:53:35 +0530

    Time: 153 ms (71.45%), Space: 11.3 MB (67.69%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -22,21 +22,4 @@ public:
         else return subsetsum(nums,sum/2);
     }
 };
-
-//  int n = nums.size();
-//         bool dp[n+1][sum + 1];
-        
-//         for(int i=0;i<=n;i++) dp[i][0] = true; //subset with 0 sum
-        
-//         for(int j=1;j<=sum;j++) dp[0][j] = false; //non-zero sum with 0 elements
-        
-//         for(int i=1;i<n+1;i++){
-//             for(int j=1;j<=sum;j++){
-//                 if(nums[i-1] > j) dp[i][j] = dp[i-1][j]; //don't include ith element if it's greater then the rem sum
-//                 else dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
-               
-//             }
-//         }
-        
-//         return dp[n][sum];
         
