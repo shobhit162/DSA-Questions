dd13bb49ef1a97ae1c180a5d48719e89d4499391
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-03-29 17:11:12 +0530

    Time: 3 ms (91.53%), Space: 9.7 MB (73.10%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        // binary search lgani h
-        bool flag=0;
-        int index=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                index= i;
-                flag=1;
-                break;
-            }
-            else if(nums[i]>target) {
-                index= i;
-                flag=1;
-                break;
-            }
+        
+         int n=nums.size();
+        int left=0,right=n;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]>=target) right=mid;
+            else left=mid+1;
         }
-       if(!flag) return nums.size();
-        else
-        return index;
+        return left;
+        
+        // binary search lgani h
+       //  bool flag=0;
+       //  int index=0;
+       //  for(int i=0;i<nums.size();i++){
+       //      if(nums[i]==target){
+       //          index= i;
+       //          flag=1;
+       //          break;
+       //      }
+       //      else if(nums[i]>target) {
+       //          index= i;
+       //          flag=1;
+       //          break;
+       //      }
+       //  }
+       // if(!flag) return nums.size();
+       //  else
+       //  return index;
     }
 };
