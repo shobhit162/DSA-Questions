2a3ede5ff81f9152db386b6a892e2d35dc56dc8a
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-04-13 00:04:16 +0530

    Time: 11 ms (86.20%), Space: 10.7 MB (63.05%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,65 +1,7 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-//           int n=s1.size(); 
-//         int m=s2.size();
-//         int t[n+1][m+1]; //table generated while computing LCS length
-//         string res; // result	
-//         // compute LCS length using tabulation      
-//     for(int i=0;i<=n;i++)
-//     {
-//         for(int j=0;j<=m;j++)
-//         {
-//             if(i==0||j==0)
-//                 t[i][j]=0;
-//             else if(s1[i-1]==s2[j-1])
-//             {
-//                 t[i][j]=1+t[i-1][j-1];
-//             }
-//             else
-//             {
-//                 t[i][j]=max(t[i-1][j],t[i][j-1]);
-//             }
-//         }
-//     }     
-//        //print lcs
-//     int i=n,j=m;
-//     while(i>0 && j>0)
-//     {
-//         if(s1[i-1]==s2[j-1])
-//         {
-//             res.push_back(s1[i-1]);
-//             i--;
-//             j--;
-//         }
-//         else
-//         {
-//             if(t[i-1][j]>t[i][j-1])
-//             {
-//                 res.push_back(s1[i-1]);
-//                 i--;
-//             } else{
-//                 res.push_back(s2[j-1]);
-//                 j--;
-//             }
-//         }
-//     }
-//     while(i>0) // if s1 characters are still left
-//     {
-//         res.push_back(s1[i-1]);
-//         i--;
-//     }
-//     while(j>0) //if s2 characters are still left
-//     {
-//         res.push_back(s2[j-1]);
-//         j--;
-        
-//     }
-//         reverse(res.begin(),res.end()); 
-//         return res;
-        
-        // bottom up dp
-        //
+      // bottom up dp
         int n=str1.size();
         int m=str2.size();
         int dp[n+1][m+1];
@@ -84,25 +26,25 @@ public:
                 i--;
                 j--;
             }
-             else
-        {
-            if(dp[i-1][j]>dp[i][j-1])
-            {
-                s.push_back(str1[i-1]);
-                i--;
-            } else{
+        //      else
+        // {
+        //     if(dp[i-1][j]>dp[i][j-1])
+        //     {
+        //         s.push_back(str1[i-1]);
+        //         i--;
+        //     } else{
+        //         s.push_back(str2[j-1]);
+        //         j--;
+        //     }
+        // }
+            else if(dp[i][j-1]>dp[i-1][j]){
                 s.push_back(str2[j-1]);
                 j--;
             }
-        }
-            // else if(dp[i][j-1]>dp[i-1][j]){
-            //     s.push_back(str2[j-1]);
-            //     j--;
-            // }
-            // else if(dp[i-1][j]>dp[i][j-1]){
-            //     s.push_back(str1[i-1]);
-            //     i--;
-            // }
+            else{
+                s.push_back(str1[i-1]);
+                i--;
+            }
         }
         while(i>0){
             s.push_back(str1[i-1]);
