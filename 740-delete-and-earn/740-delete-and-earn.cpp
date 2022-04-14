5cd0cbfbbf31c084b900398e60051dba4b9410f7
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-04-14 20:15:19 +0530

    Time: 15 ms (42.19%), Space: 13.9 MB (27.63%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,62 +1,31 @@
 class Solution {
 public:
-   
-     int t[20001]; // declaring 1 d array, to store some already computed results
-    int solve(vector<int> &arr, int i)
-    {
-        if(i >= arr.size()) // if i is greater than size of array
-        {
-            return 0; // then simply returnn zero
+    int deleteAndEarn(vector<int>& nums) {
+        int n=10001;
+        vector<int>points(n);
+        for(auto i:nums)points[i]+=i;
+        int dp[n];
+        dp[0]=0;
+        dp[1]=points[1];
+        for(int i=2;i<n;i++){
+            dp[i]=max(points[i]+dp[i-2],dp[i-1]);
         }
-        
-         // if result is already computed, then return from here
-        // this one line which we say to add
-        if(t[i] != -1)
-        {
-            return t[i];
-        }
-        
-        // current 'i' on which we are standing
-        int currValue = arr[i];  // current value
-        int currSum = arr[i]; // intial make sum as same as value
-        int index = i + 1; // index to take elemets, so  i + 1
-        
-        // while it is the same as the current value, include in our sum
-        while(index < arr.size() && arr[index] == currValue)
-        {
-            currSum += arr[i];
-            index++;
-        }
-        
-        // Now, we have to skip all the elements, whose value is equal to
-        // currValue + 1
-        while(index < arr.size() && arr[index] == currValue + 1)
-        {
-            index++;
-        }
-        
-        //And lastly, we have two choices-
-        //whether to include the sum of this current element in our answer
-        // or not include the sum of current element in our answer
-        // so we explore all possibility and take maximum of them
-        
-        return t[i] = max(currSum + solve(arr, index), solve(arr, i + 1));
-        
-        // If we decide to take the curr element in our answer, then upto the elemet we skip the next value, we paas that index
-        // but if decided no to make this vurrent element then simply paas
-        // i + 1
-    }
-    int deleteAndEarn(vector<int>& arr) {
-        int n = arr.size(); // take the size of the array
-        
-        memset(t, -1, sizeof(t));
-        
-        // sort the array to get rid of all arr[i] - 1 elements
-        sort(arr.begin(), arr.end());
-        
-        // solve function which give us our final answer
-        return solve(arr, 0);
-        //                ↑
-        //                we start from zero index
+        return dp[n-1];
     }
 };
+
+// vector<int> sum(n, 0);
+//     vector<int> dp(n, 0);
+    
+//     for(auto num: nums){
+//         sum[num] += num;
+//     }
+    
+//     dp[0] = 0;
+//     dp[1] = sum[1];
+//     //now apply the house robbing concept
+//     for(int i=2; i<n; i++){
+//         dp[i] = max(dp[i-2] + sum[i], dp[i-1]);
+//     }
+    
+//     return dp[n-1];
