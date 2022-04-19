1fb12bc8b724e80a86c8852163e3be4c0dd073ba
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-04-19 17:58:17 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (72.89%) - LeetHub

--- 2243-calculate-digit-sum-of-a-string/2243-calculate-digit-sum-of-a-string.cpp
+++ 2243-calculate-digit-sum-of-a-string/2243-calculate-digit-sum-of-a-string.cpp
@@ -23,23 +23,3 @@ public:
         return s;
     }
 };
-
-  // while ((int) s.size() > k) {
-  //           int sum = 0;
-  //           string t = "";
-  //           int count = 0;
-  //           for (int i = 0; i < (int) s.size(); i++) {
-  //               sum += (s[i] - '0');
-  //               ++count;
-  //               if (count == k) {
-  //                   t += to_string(sum);
-  //                   sum = 0;
-  //                   count = 0;
-  //               }
-  //           }
-  //           if (count > 0) {
-  //               t += to_string(sum);
-  //           }
-  //           s = t;
-  //       }
-  //       return s;
