db39dd4b4d660494b8e287155a3260fcf3921eb9
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-05-24 10:41:55 +0530

    Added solution - LeetHub

--- Array Partition - GFG/array-partition.cpp
+++ Array Partition - GFG/array-partition.cpp
@@ -7,24 +7,73 @@ using namespace std;
  // } Driver Code Ends
 //User function Template for C++
 
-class Solution{
+ class Solution{
     public:
+    int dp[200005];
+    int count[200005];
     bool partitionArray(int N, int K, int M, vector<int> &A){
-        // code here
-         bool dp[N+1] = {0};
-        dp[0] = 1;
-        sort(A.begin(), A.end());
-        for(int i=K; i<=N; i++){
-            int l = lower_bound(A.begin(), A.end(), A[i-1] - M) - A.begin();
-            int h = i - K;
-            for(int j=l; j<=h; j++){
-                dp[i] |= dp[j];
-                if (dp[i])
-                    break;
+        memset(dp , -1 , sizeof(dp));
+        memset(count , 0 , sizeof(count));
+        sort(A.begin(),A.end());
+        int n = A.size();
+        
+        
+        for(int i=0; i< N; i++){
+            int pos = search(A, i , N , A[i] + M);
+            int cnt = (pos -i) + 1;
+            count[i] = cnt;
+        }
+        
+        help(0 , N , A , K , M);
+        return dp[0];
+    }
+    
+    int search( vector<int> &a , int st ,int en , int tar){
+        int res = st;
+        while(st <= en){
+            int mid = (st+en)/2;
+            //cout<<a[mid]<<" "<<tar<<endl;
+            if(a[mid] <= tar){
+                res = mid;
+                st = mid+1;
+            } 
+            else{
+                en = mid-1;
             }
         }
-        return dp[N];
+       
+        return res;
     }
+    
+    int help(int idx , int n , vector<int> &a , int k, int m){
+        
+        if(idx >= n) return 1;
+    
+        if(dp[idx] != -1) return dp[idx];
+        
+        
+        int cnt = count[idx];
+        
+        if(cnt < k) {
+            
+            dp[idx] = 0;
+            return 0;
+        }
+        
+        int ans = 0;
+        for(int i= idx+k; i<= idx+cnt; i++ ){
+          int tem= help(i, n ,a , k , m );
+          if(tem == 1){
+              ans = 1;
+              break;
+          }
+            
+        }
+        return dp[idx] = ans;
+        
+    }
+    
+    
 };
 
 // { Driver Code Starts.
