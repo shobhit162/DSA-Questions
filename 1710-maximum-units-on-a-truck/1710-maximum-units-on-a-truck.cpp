5cc57eac5b14767610666770b18960413f443f4c
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-07-01 13:08:15 +0530

    Time: 47 ms (88.28%), Space: 15.9 MB (95.14%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -8,16 +8,23 @@ public:
         sort(boxTypes.begin(),boxTypes.end(),sorttype);
         int ans=0;
         int count=0;
-        for(auto i:boxTypes)cout<<i[0]<<" "<<i[1]<<endl;
-        for(int i=0;i<boxTypes.size();i++){
-            if(count>=truckSize)break;
-            while(boxTypes[i][0]!=0 && count!=truckSize){
-                count++;
-                ans+=boxTypes[i][1];
-                boxTypes[i][0]--;
-            }
+//         for(auto i:boxTypes)cout<<i[0]<<" "<<i[1]<<endl;
+//         for(int i=0;i<boxTypes.size();i++){
+//             if(count>=truckSize)break;
+//             while(boxTypes[i][0]!=0 && count!=truckSize){
+//                 count++;
+//                 ans+=boxTypes[i][1];
+//                 boxTypes[i][0]--;
+//             }
             
+//         }
+        int maxUnits=0;
+        for(auto& box : boxTypes) {
+		if(truckSize <= 0) break;                    // keep choosing greedily till you run out of truckSize 
+		maxUnits += min(truckSize, box[0]) * box[1]; // add (no of box * units) in that box
+		truckSize -= box[0];
         }
-        return ans;
+        // return ans;
+            return maxUnits;
     }
 };
