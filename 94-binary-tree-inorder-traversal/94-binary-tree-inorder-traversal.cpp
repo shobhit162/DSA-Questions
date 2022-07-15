ac700ed21f8cf19f04230416a6ae610bfb4f80b5
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-07-15 16:13:17 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (40.38%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -11,18 +11,33 @@
  */
 class Solution {
 public:
-    vector<int> inorderTraversal(TreeNode* root) {
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> nodes;
+//         inorder(root, nodes);
+//         return nodes;
+//     }
+// private:
+//     void inorder(TreeNode* root, vector<int>& nodes) {
+//         if (!root) {
+//             return;
+//         }
+//         inorder(root -> left, nodes);
+//         nodes.push_back(root -> val);
+//         inorder(root -> right, nodes);
+//     }
+     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> nodes;
-        inorder(root, nodes);
-        return nodes;
-    }
-private:
-    void inorder(TreeNode* root, vector<int>& nodes) {
-        if (!root) {
-            return;
+        stack<TreeNode*> todo;
+        while (root || !todo.empty()) {
+            while (root) {
+                todo.push(root);
+                root = root -> left;
+            }
+            root = todo.top();
+            todo.pop();
+            nodes.push_back(root -> val);
+            root = root -> right;
         }
-        inorder(root -> left, nodes);
-        nodes.push_back(root -> val);
-        inorder(root -> right, nodes);
+        return nodes;
     }
 };
