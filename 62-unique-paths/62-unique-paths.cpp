f8989f28ddbdd61cd350a71917fdf1366cd5d2df
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-08-01 10:35:24 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (63.21%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-         vector<vector<int>> dp(m, vector<int>(n, 1));
+        //  vector<vector<int>> dp(m, vector<int>(n, 1));
+        // for(int i = 1; i < m; i++)
+        //     for(int j = 1; j < n; j++)
+        //         dp[i][j] = dp[i-1][j] + dp[i][j-1];   // sum of unique paths ending at adjacent top and left cells
+        // return dp[m-1][n-1];         // return unique paths ending at cell (m-1, n-1)
+        
+           vector<int> dp(n, 1);  // space optimized
         for(int i = 1; i < m; i++)
             for(int j = 1; j < n; j++)
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];   // sum of unique paths ending at adjacent top and left cells
-        return dp[m-1][n-1];         // return unique paths ending at cell (m-1, n-1)
+                dp[j] += dp[j-1];   
+        return dp[n-1];
     }
 };
