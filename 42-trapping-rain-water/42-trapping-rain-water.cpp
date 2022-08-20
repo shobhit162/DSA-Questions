423914444e5a5e05875ae61034f85966e3d10e6d
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-08-20 10:36:17 +0530

    Time: 20 ms (74.68%), Space: 19.8 MB (61.26%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-         int n = height.size();
-  int prefix[n], suffix[n];
-  prefix[0] = height[0];
-  for (int i = 1; i < n; i++) {
-    prefix[i] = max(prefix[i - 1], height[i]);
+  //        int n = height.size();     // O(3n)
+  // int prefix[n], suffix[n];
+  // prefix[0] = height[0];
+  // for (int i = 1; i < n; i++) {
+  //   prefix[i] = max(prefix[i - 1], height[i]);
+  // }
+  // suffix[n - 1] = height[n - 1];
+  // for (int i = n - 2; i >= 0; i--) {
+  //   suffix[i] = max(suffix[i + 1], height[i]);
+  // }
+  // int waterTrapped = 0;
+  // for (int i = 0; i < n; i++) {
+  //   waterTrapped += min(prefix[i], suffix[i]) - height[i];
+  // }
+  // return waterTrapped;
+        
+         int n = height.size();    // O(n)
+  int left = 0, right = n - 1;
+  int res = 0;
+  int maxLeft = 0, maxRight = 0;
+  while (left <= right) {
+    if (height[left] <= height[right]) {
+      if (height[left] >= maxLeft) {
+        maxLeft = height[left];
+      } else {
+        res += maxLeft - height[left];
+      }
+      left++;
+    } else {
+      if (height[right] >= maxRight) {
+        maxRight = height[right];
+      } else {
+        res += maxRight - height[right];
+      }
+      right--;
+    }
   }
-  suffix[n - 1] = height[n - 1];
-  for (int i = n - 2; i >= 0; i--) {
-    suffix[i] = max(suffix[i + 1], height[i]);
-  }
-  int waterTrapped = 0;
-  for (int i = 0; i < n; i++) {
-    waterTrapped += min(prefix[i], suffix[i]) - height[i];
-  }
-  return waterTrapped;
+  return res;
     }
 };
