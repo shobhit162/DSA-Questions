400c8f675a90291bb4eb6ab4da4986f1a46f310e
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-10-10 09:18:23 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
-    string breakPalindrome(string p) {
-        string ans="";
-        if(p.size()==1)return ans;
-        int mid=p.size();
-        if(mid%2!=0){
-            mid/=2;
-            mid+=1;
-        }
-        for(int i=0;i<p.size();i++){
-            if(p.size()%2!=0 &&i==mid-1)continue;
-            if(p[i]!='a'){
-                ans+=p.substr(0,i);
-               ans+="a";
-                ans+=p.substr(i+1);
-                break;
+    // string breakPalindrome(string p) {
+    //     string ans="";
+    //     if(p.size()==1)return ans;
+    //     int mid=p.size();
+    //     if(mid%2!=0){
+    //         mid/=2;
+    //         mid+=1;
+    //     }
+    //     for(int i=0;i<p.size();i++){
+    //         if(p.size()%2!=0 &&i==mid-1)continue;
+    //         if(p[i]!='a'){
+    //             ans+=p.substr(0,i);
+    //            ans+="a";
+    //             ans+=p.substr(i+1);
+    //             break;
+    //         }
+    //          if(i==p.size()-1 && p[i]=='a'){
+    //             ans+=p.substr(0,i);
+    //             ans+="b";
+    //         }
+    //     }
+    //     return ans;
+    // }
+     string breakPalindrome(string S) {
+        int n = S.size();
+        for (int i = 0; i < n / 2; ++i) {
+            if (S[i] != 'a') {
+                S[i] = 'a';
+                return S;
             }
-             if(i==p.size()-1 && p[i]=='a'){
-                ans+=p.substr(0,i);
-                ans+="b";
-            }
         }
-        return ans;
+        S[n - 1] = 'b';
+        return n < 2 ? "" : S;
     }
 };
