111119f0f5fa8eba5ef498456c7e7263f3a28ee8
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-10-19 10:25:09 +0530

    Time: 36 ms (15.60%), Space: 12.8 MB (39.56%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,33 +1,64 @@
 class Solution {
 public:
+//     vector<string> topKFrequent(vector<string>& words, int k) {
+//         map<string,int> m;
+//         vector<int> v;
+//         vector<string> res;
+//         for(auto x:words)
+//         {
+//             m[x]++;
+//         }
+        
+//         for(auto x:m)
+//             {
+//                 v.push_back(x.second);
+
+//             }
+//         sort(v.begin(),v.end(),greater<int>());
+//         for(int i=0;i<k;i++)
+//         {   int temp=v[i];
+//             for(auto x:m)
+//             {
+//                 if(x.second==temp)
+//                     {res.push_back(x.first);
+//                     m.erase(x.first);
+//                     break;
+//                     }
+//             }
+            
+          
+//         }
+//       return res;  
+//     }
+    
+     static bool comparator(pair<string,int>& a, pair<string,int>& b){
+        if(a.second != b.second){
+            return a.second > b.second;
+        }else{
+            return a.first < b.first;
+        }
+    }
+    
     vector<string> topKFrequent(vector<string>& words, int k) {
         map<string,int> m;
-        vector<int> v;
-        vector<string> res;
-        for(auto x:words)
-        {
+        for(auto x : words){
             m[x]++;
         }
         
-        for(auto x:m)
-            {
-                v.push_back(x.second);
-
-            }
-        sort(v.begin(),v.end(),greater<int>());
-        for(int i=0;i<k;i++)
-        {   int temp=v[i];
-            for(auto x:m)
-            {
-                if(x.second==temp)
-                    {res.push_back(x.first);
-                    m.erase(x.first);
-                    break;
-                    }
-            }
-            
-          
+        vector<pair<string,int>> newV;
+        for(auto x : m){
+            pair p{x.first, x.second};
+            newV.push_back(p);
         }
-      return res;  
+        
+        sort(newV.begin(),newV.end(),comparator);
+        
+        vector<string> ans;
+        for(int i = 0; i < k; i++){
+            ans.push_back(newV[i].first);
+        }
+        
+        return ans;
+        
     }
 };
