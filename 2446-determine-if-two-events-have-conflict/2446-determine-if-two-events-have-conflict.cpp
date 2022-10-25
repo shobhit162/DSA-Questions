10638851e225e1b190e4c98440935249052c08b5
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-10-25 10:50:41 +0530

    Time: 7 ms (30.55%), Space: 11.5 MB (42.53%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
@@ -25,7 +25,10 @@ public:
     //     // cout<<x<<" "<<y<<" "<<a<<" "<<b<<" ";
     //     return false;
     // }
-      bool haveConflict(vector<string>& e1, vector<string>& e2) {
-        return e1[0] <= e2[1] && e2[0] <= e1[1];
+    //   bool haveConflict(vector<string>& e1, vector<string>& e2) {
+    //     return e1[0] <= e2[1] && e2[0] <= e1[1];
+    // }
+        bool haveConflict(vector<string>& e1, vector<string>& e2) {
+        return max(e1[0], e2[0]) <= min(e1[1], e2[1]);
     }
 };
