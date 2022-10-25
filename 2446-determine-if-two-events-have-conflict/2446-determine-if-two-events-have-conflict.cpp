5d7d5d42c90a6e1bf7d5a5a0807d03b69ff1c1a1
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-10-25 10:50:12 +0530

    Time: 0 ms (100.00%), Space: 11.5 MB (42.53%) - LeetHub

---
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    // bool haveConflict(vector<string>& event1, vector<string>& event2) {
+    //     string s1=event1[0];
+    //     string s2=event1[1];
+    //     string s3=event2[0];
+    //     string s4=event2[1];
+    //     string ss1="";
+    //     ss1+=s1.substr(0,2);
+    //     ss1+=s1.substr(3,4);
+    //     string ss2="";
+    //     ss2+=s2.substr(0,2);
+    //     ss2+=s2.substr(3,4);
+    //     string ss3="";
+    //     ss3+=s3.substr(0,2);
+    //     ss3+=s3.substr(3,4);
+    //     string ss4="";
+    //     ss4+=s4.substr(0,2);
+    //     ss4+=s4.substr(3,4);
+    //     int x=stoi(ss1);
+    //     int y=stoi(ss2);
+    //     int a=stoi(ss3);
+    //     int b=stoi(ss4);
+    //     if((a>=x && a<=y) || (b>=x && b<=y) || (x>=a && x<=b) || (y>=a && y<=b))return true;
+    //     // cout<<x<<" "<<y<<" "<<a<<" "<<b<<" ";
+    //     return false;
+    // }
+      bool haveConflict(vector<string>& e1, vector<string>& e2) {
+        return e1[0] <= e2[1] && e2[0] <= e1[1];
+    }
+};
