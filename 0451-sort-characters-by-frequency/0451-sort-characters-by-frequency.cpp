426949dcd69587ee8e426acfdbdf1d427e8fe4c6
Shobhit Chaurasia <69913411+shobhit162@users.noreply.github.com>
2022-12-03 11:31:23 +0530

    Time: 49 ms (10.20%), Space: 15.7 MB (5.02%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string frequencySort(string s) {
+    string frequencySort(string s) {  // O(n)
         // unordered_map<char,int> freq;           //for frequency of characters
         // priority_queue<pair<int,char>> maxheap; //maxheap according to frequency of characters
         // for(char c: s)
@@ -14,14 +14,35 @@ public:
         // }
         // return s;
         
-        // using lambda, comparator function
+        // using lambda, comparator function  O(nlogn)
         
-         int counts[256] = {0};
-        for (char ch : s)
-            ++counts[ch];
-        sort(s.begin(), s.end(), [&](char a, char b) { 
-            return counts[a] > counts[b] || (counts[a] == counts[b] && a < b); 
-        });
-        return s;
+    //      int counts[256] = {0};
+    //     for (char ch : s)
+    //         ++counts[ch];
+    //     sort(s.begin(), s.end(), [&](char a, char b) { 
+    //         return counts[a] > counts[b] || (counts[a] == counts[b] && a < b); 
+    //     });
+    //     return s;
+    // }
+        
+        // using bucket sort  O(n)
+         unordered_map<char,int> freq;
+        vector<string> bucket(s.size()+1, "");
+        string res;
+        
+        //count frequency of each character
+        for(char c:s) freq[c]++;
+        //put character into frequency bucket
+        for(auto& it:freq) {
+            int n = it.second;
+            char c = it.first;
+            bucket[n].append(n, c);
+        }
+        //form descending sorted string
+        for(int i=s.size(); i>0; i--) {
+            if(!bucket[i].empty())
+                res.append(bucket[i]);
+        }
+        return res;
     }
 };
